480e15081ad66892c404e22da04e5d1f5b827009
LAGOM-YOLO-GX330 <76835550+LAGOM-YOLO-GX330@users.noreply.github.com>
2024-12-01 10:38:53 +0800

    Add files via upload

---
+++ untitled/src/test.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class test {
+       public static void main(String[] args) {
+           int i=1;
+           while(i<=100){
+               if(i%3==0&&i%5==0){
+                   System.out.println(i);
+               }
+               ++i;
+           }
+
+       }
+}
